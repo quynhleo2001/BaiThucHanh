f8317e724062f7946ce9351153552192cd254735
quynhleo2001 <quynhleo2001@gmail.com>
2023-03-12 17:46:37 +0700

    BaiThucHanh0703

--- BaiThucHanh0703/Controllers/StudentController.cs
+++ BaiThucHanh0703/Controllers/StudentController.cs
@@ -34,6 +34,43 @@ namespace BaiThucHanh0703.Controllers
 
          return View();
        }
+       public IActionResult GiaiPhuongTrinhBac2()
+       {
+        return View();
+       }
+       [HttpPost]
+        public IActionResult GiaiPhuongTrinhBac2(string hesoA, string hesoB, string hesoC)
+       {
+        //khai bao bien
+        double delta , x1, x2, a=0, b=0, c=0;
+        string ketqua;
+        //giai phuong trinh bac 2
+        if(!String.IsNullOrEmpty(hesoA)) a = Convert.ToDouble(hesoA);
+         if(!String.IsNullOrEmpty(hesoB)) b = Convert.ToDouble(hesoB);
+          if(!String.IsNullOrEmpty(hesoC)) c = Convert.ToDouble(hesoC);
+        if(a == 0) ketqua = "Khong phai phuong trinh bac 2";
+        else
+        {
+          //tinh delta
+          delta = Math.Pow(b,2) - 4*a*c;
+          //giai phuong trinh
+          if(delta < 0) ketqua = "Phuong trinh vo nghiem";
+          else if(delta == 0)
+          {
+            x1 = -b/(2*a);
+            ketqua = "Phuong trinh co nghiem kep = " + x1;
+          }
+          else
+          {
+            x1 = (-b + Math.Sqrt(delta))/(2*a);
+            x2 = (-b - Math.Sqrt(delta))/(2*a);
+            ketqua = "Phuong trinh co 2 nghiem phan biet: x1 =" + x1 + ", x2 = " + x2;
+          }
+        }
+        @ViewBag.message = ketqua;
+        return View();
+       }
+
 
 
 
